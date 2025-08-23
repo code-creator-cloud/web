ab46212d59de828c074910dbebca5852104639b2
code-creator-cloud <testerwizard02@gmail.com>
2025-08-23 13:40:25 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 
 import './App.css'
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
      
-        <h1 className='text-red-500'>helo</h1>
+        <h1 className='text-red-500'>hello</h1>
     </>
   )
 }
